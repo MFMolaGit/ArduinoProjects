97e093a373773bb29242e8d90de3a1f2bdd87787
Paco Navarro <packarbell@gmail.com>
2018-01-28 20:14:35 +0100

    Pata

--- trasto/pata.cpp
+++ trasto/pata.cpp
@@ -1,17 +1,3 @@
-#include <Servo.h>
 
-class Pata {
-  private:
-    Servo servo1;
-  public:
-    Pata(int);
-    void move(int n);
-};
 
-Pata::Pata(int pin) {
-  servo1.attach(pin);
-}
 
-void Pata::move(int n) {
-  servo1.write(n);
-};
--- trasto/trasto.ino
+++ trasto/trasto.ino
@@ -1,14 +1,57 @@
-#include "pata.cpp"
- 
-Pata pata(11);    // Nombre asignado al servo
-int angulo;                // Variable de almacenamiento del angulo ingresado por Monitor Serial
+#include <Servo.h>
+
+class Pata {
+  private:
+    int pin;
+    int pos;
+  public:
+    Pata(int);
+    void move(int n);
+    void init();
+};
+
+Servo servo1;
+int angulo;
+
+
+Pata::Pata(int n) {
+  pin = n;
+}
+
+void Pata::init() {
+  servo1.attach(pin);
+  pos = 90;
+  servo1.write(pos);
+}
+
+void Pata::move(int n) {
+  for (int i=0; i<abs(pos-n); i++){
+    delay(7);
+    if(pos > n) {
+        servo1.write(pos - i);
+    } else {
+        servo1.write(pos + i);
+    }
+  }
+  pos = n;
+};
+
+Pata pata(11);
+
+void setup() {
+  Serial.begin(9600);
+  pata.init();
+  // Mensaje inicial en el monitor serial
+  Serial.println("Escribir la posicion de angulo de 0 a 180: ");
+}
 
 void loop() {
   if (Serial.available()) {   // Verificacion que el puerto serial recibe datos                                  
-   delay(10);               
+   delay(10);
+ 
    angulo = Serial.parseInt(); // Lee solo los datos tipo int del buffer
-   //pata.move(angulo); // Escritura de microsegundos ingresado por Monitor Serial
-    
+   pata.move(angulo); // Escritura de microsegundos ingresado por Monitor Serial
+   
    // Mensaje de confirmacion del angulo ingresado por Monitor Serial
    Serial.print("Posicion angular: "); 
    Serial.print(angulo);
@@ -17,3 +60,4 @@ void loop() {
     while (Serial.available() > 0){Serial.read();} // Rutina de limpieza del buffer del puerto serial
   }
 }
+
