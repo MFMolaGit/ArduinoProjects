5c71c117c71901dff8148d290464c66926704bcf
Manuel Francisco Mola <manuelfrancisco.mola@bbva.com>
2018-02-24 18:36:50 +0100

    Trazas con directiva de preprocesamiento

--- Trasto/Cuerpo.cpp
+++ Trasto/Cuerpo.cpp
@@ -4,28 +4,38 @@
 Cuerpo::Cuerpo() {}
 
 Cuerpo::Cuerpo(int n, int m, int **pinsPorPata) {
-  //Serial.println("Instanciando cuerpo");
+  
+  #ifdef TRAZAS
+  Serial.println("Instanciando cuerpo con ");
+  Serial.print(n);
+  Serial.print(" Patas y ");
+  Serial.print(m);
+  Serial.println(" Ejes por Pata");
+  #endif
+  
   numPatas = n;
   patas = new Pata*[numPatas];
   for (int i = 0; i < numPatas; i++) {
-    patas[i] = new Pata(m,pinsPorPata[i]);
+    patas[i] = new Pata(i+1, m, pinsPorPata[i]);
   }
 }
 
 void Cuerpo::init() {
-  //Serial.println("Inicializando cuerpo");
+  
+  #ifdef TRAZAS
+  Serial.println("Inicializando cuerpo");
+  #endif
+  
   for (int i = 0; i < numPatas; i++) {
       patas[i]->init();
   }
 }
 
 void Cuerpo::move(int numPata, int numEje, int nuevaPos) {
-  //Serial.println("Moviendo cuerpo");
-  /*Serial.print("Moviendo Eje ");
-  Serial.print(numEje);
-  Serial.print(" de Pata ");
-  Serial.print(numPata);
-  Serial.print(" a posicion ");
-  Serial.println(nuevaPos);*/
+  
+  #ifdef TRAZAS
+  Serial.println("Moviendo cuerpo");
+  #endif
+  
   patas[numPata-1]->move(numEje, nuevaPos);  
 };
--- Trasto/Pata.hpp
+++ Trasto/Pata.hpp
@@ -2,10 +2,11 @@
 
 class Pata {
   private:
-    Eje** ejes;
+    int id;
+    Eje** ejes;   
     int numEjes;
   public:
-    Pata(int, int*);
+    Pata(int, int, int*);
     void init();    
     void move(int, int);
 };
